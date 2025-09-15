2b9561553292733c5aa75474d4c0c03db43ec628
Gabriel Martins <gabrielmartins8802@gmail.com>
2025-09-15 18:11:02 -0300

    Adicionado arquivo para um mini banco de dados

---
+++ JavaScript/detalhes.js
@@ -0,0 +1,2 @@
+//aqui vamos usar para guardar infos completas, que so importam quando o usuario abre a tela do carro (mesmo id do lista-car, desc, preço, cor e imagem em link ou caminho do archive)
+
