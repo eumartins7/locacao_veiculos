14fd41a304ab97d2ebcc5d9b03543b960555009c
Gabriel Martins <gabrielmartins8802@gmail.com>
2025-09-15 18:09:37 -0300

    Adicionado lista de carros

---
+++ JavaScript/lista-car.js
@@ -0,0 +1,9 @@
+//aqui vamos guardar as infos essenciais de cada carro (id, modelo e ano) com array
+
+let carros = [
+    {id: "uno2005", modelo: "Uno", ano: 2005},
+    {id: "gol2018", modelo: "Gol", ano: 2018},
+    {id: "onix2020", modelo: "Onix", ano: 2020},
+    {id: "hb2017", modelo: "Hb20", ano: 2017},
+    {id: "kwid2019", modelo: "Kwid", ano: 2019}
+]
