f36f40e9095f36b41b464d9499da16662032b8d7
Gabriel Martins <gabrielmartins8802@gmail.com>
2025-09-16 00:42:12 -0300

    adicionado mini banco de dados dos carros

--- JavaScript/detalhes.js
+++ JavaScript/detalhes.js
@@ -1,2 +1,37 @@
 //aqui vamos usar para guardar infos completas, que so importam quando o usuario abre a tela do carro (mesmo id do lista-car, desc, preço, cor e imagem em link ou caminho do archive)
 
+let detalhes = [
+    {
+        id: "uno2005", 
+        descricao: "Carro compacto, econômico e fácil de estacionar. Ideal para uso diário na cidade.",
+        preco: 2.000 , 
+        cor: "prata",
+        img: "imagens/cars/uno.jpg"
+    },
+    {
+        id: "gol2018",
+        descricao: "Popular no Brasil, bom desempenho e manutenção acessível. Versátil para diversas situações.",
+        preco: 2.000 ,
+        cor: "prata",
+        img: "imagens/cars/gol.png"
+    },
+    {
+        id: "onix2020", 
+        descricao: "Modelo moderno, com bom consumo de combustível e tecnologia atualizada.",
+        preco: 2.000 , 
+        cor: "prata", 
+        img: "imagens/cars/onix.png"
+    },
+    {id: "hb2017", 
+        descricao: "Design atualizado, confortável e com bom espaço interno para a categoria.",
+        preco: 2.000 , 
+        cor: "prata", 
+        img: "imagens/cars/hb20.webp"
+    },
+    {id: "kwid2019", 
+        descricao: "Um dos carros mais econômicos do mercado, compacto e barato de manter.",
+        preco: 2.000 ,
+        cor: "prata", 
+        img: "imagens/cars/kwid.png"
+    }
+]
