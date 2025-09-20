4f5859e9c571c5f42acb01cf2e62ddb326b4c3b0
Gabriel Martins <gabrielmartins8802@gmail.com>
2025-09-20 18:32:08 -0300

    estrutura atualizada

--- js/tela-detalhes.js
+++ js/tela-detalhes.js
@@ -12,7 +12,7 @@ botao.forEach(botao => { //botao, variavel temporaria na function que recebe cad
     let idCarro = botao.getAttribute('data-id')
 
     //template string. sem muito o que falar
-    let novaUrl = `../page.aluguel/index.html?id=${idCarro}`
+    let novaUrl = `../public.aluguel/index.html?id=${idCarro}`
 
     //aqui cria ou substitui o atributo href no botao. Se o <a> estava sem link, passa a ter
     botao.setAttribute('href', novaUrl)
